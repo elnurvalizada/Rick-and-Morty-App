5580a1ef47f8bcfb5cb505cadfe216ef790070fd
Elnur Valizada <elnurvalizada@Elnurs-iMac.local>
2025-06-04 10:13:56 +0400

    Updates

--- RickandMorty/Core/Repo/CharacterService.swift
+++ RickandMorty/Core/Repo/CharacterService.swift
@@ -8,37 +8,37 @@
 import Foundation
 
 final class CharacterService {
-  private let networkService : NetworkService
-  
-  init(networkService: NetworkService = NetworkManager.shared) {
-    self.networkService = networkService
-  }
-  
-  func fetchCharacters(
-    name: String?,
-    currentFilters: [String?: String?],
-    page: Int,
-    completion: @escaping (Result<CharacterResponse, NetworkError>) -> Void
-  ) {
-    var queryItems = [URLQueryItem(name: "page", value: "\(page)")]
+    private let networkService : NetworkService
     
-    if let name = name, !name.isEmpty {
-      queryItems.append(URLQueryItem(name: "name", value: name))
-    }
-    currentFilters.forEach { key, value in
-      if let value = value, !value.isEmpty, let key = key, !key.isEmpty {
-        queryItems.append(URLQueryItem(name: key.lowercased(), value: value.lowercased()))
-      }
+    init(networkService: NetworkService = NetworkManager.shared) {
+        self.networkService = networkService
     }
     
-    print(queryItems)
-    let endpoint = Endpoint(
-      baseURL: "https://rickandmortyapi.com/api",
-      path: "/character",
-      queryItems: queryItems
-    )
+    func fetchCharacters(
+        name: String?,
+        currentFilters: [String?: String?],
+        page: Int,
+        completion: @escaping (Result<CharacterResponse, NetworkError>) -> Void
+    ) {
+        var queryItems = [URLQueryItem(name: "page", value: "\(page)")]
+        
+        if let name = name, !name.isEmpty {
+            queryItems.append(URLQueryItem(name: "name", value: name))
+        }
+        currentFilters.forEach { key, value in
+            if let value = value, !value.isEmpty, let key = key, !key.isEmpty {
+                queryItems.append(URLQueryItem(name: key.lowercased(), value: value.lowercased()))
+            }
+        }
+        
+        print(queryItems)
+        let endpoint = Endpoint(
+            baseURL: "https://rickandmortyapi.com/api",
+            path: "/character",
+            queryItems: queryItems
+        )
+        
+        networkService.request(endpoint: endpoint, completion: completion)
+    }
     
-    networkService.request(endpoint: endpoint, completion: completion)
-  }
-  
 }
